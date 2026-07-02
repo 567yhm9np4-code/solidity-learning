15265c560ff1322cc8a04ccddc220c3b3aa5e0fa
Geonwookim <kimgeonwoo0815@gmail.com>
2026-07-02 23:51:15 +0900

    added approve and transferFrom

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,12 +2,17 @@
 pragma solidity ^0.8.28;
 
 contract MyToken {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed spender, uint256 amount);
+
     string public name;
     string public symbol;
     uint8 public decimals;
 
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
+    mapping (address => mapping(address => uint256)) public allowance;
+    
 
     constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _amount) {
         name = _name;
@@ -18,11 +23,27 @@ contract MyToken {
         _mint(_amount * 10 ** uint256(decimals), msg.sender); // 1 MT
     }
     
+    function approve(address spender, uint256 amount) external {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(spender, amount);
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external {
+        address spender = msg.sender;
+        require(allowance[from][spender] >= amount, "Insufficient allowance");
+        allowance[from][spender] -= amount;
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
     function _mint(uint256 amount, address owner) internal {
         // totalSupply = totalSupply + amount;
         // balanceOf[owner] = balanceOf[owner] + amount;
         totalSupply += amount;
         balanceOf[owner] += amount;
+        emit Transfer(address(0), owner, amount);
+
     }
 
 
@@ -31,18 +52,18 @@ contract MyToken {
 
         balanceOf[msg.sender] -= amount;
         balanceOf[to] += amount;
+
+
+        emit Transfer(msg.sender, to, amount);
     }
-
-
-    // function totalSupply() external view returns (uint256) {
-    //    return totalSupply;
-    // }
-
-    // function balanceOf(address owner) external view returns (uint256) {
-    //     return balanceOf[owner];
-    // }
-
-    // function name() external view returns (string memory) {
-    //     return name;
-    // }
 }
+
+
+
+
+
+
+
+
+
+
